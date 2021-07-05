985b57e2d108d0b554013bc7c77c8e16fef0d61c
Pincodeert <84202600+Pincodeert@users.noreply.github.com>
2021-07-05 23:35:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// Dit is deel 1 van opdracht 3
+const age = 52
+console.log(age)
+if (age > 17) {
+    console.log("Je bent 18 jaar of ouder en je mag naar binnen");
+    
+}
+if (age < 18 ){
+    console.log("Je bent helaas nog te jong en wordt vriendelijk verzocht buiten te blijven");
+}
+
+// Dit is deel 2 van opdracht 3
+const isFemale = false;
+if (isFemale) {
+    console.log("Welkom lady, op de ladiesnight");
+}
+else {
+    console.log("Sorry, vanavond zijn we alleen open voor ladies");
+}
+
+// Dit is deel 3 van opdracht 3
+const driverStatus = "BOB";
+if (driverStatus ="BOB") {
+    console.log("Geweldig! 0% op, 100% BOB");
+}
+else { 
+    console.log("Dat wordt niet meer rijden voor jou. Zal ik een taxi voor je bellen?");
+}
